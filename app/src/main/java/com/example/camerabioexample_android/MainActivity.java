c004631335ac308ae5c2144f524d5f8b3af81900
Eduardo C <eduardoc_jr@me.com>
2021-04-15 18:03:11 -0300

    seila

--- app/src/main/java/com/example/camerabioexample_android/MainActivity.java
+++ app/src/main/java/com/example/camerabioexample_android/MainActivity.java
@@ -8,8 +8,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.camerabioexample_android.camerabiomanager.CallbackCameraBio;
-import com.example.camerabioexample_android.camerabiomanager.CameraBioManager;
+import com.example.camerabioandroid.camerabiomanager.CallbackCameraBio;
+import com.example.camerabioandroid.camerabiomanager.CameraBioManager;
 
 public class MainActivity extends AppCompatActivity implements CallbackCameraBio {
 
--- app/src/main/java/com/example/camerabioexample_android/camerabiomanager/camera/CaptureImageProcessor.java
+++
@@ -1,6 +0,0 @@
-package com.example.camerabioexample_android.camerabiomanager.camera;
-
-public interface CaptureImageProcessor {
-
-    void capture(String base64);
-}
